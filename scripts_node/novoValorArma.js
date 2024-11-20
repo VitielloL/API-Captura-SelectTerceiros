ca50262cad52ef64672d75fdcc89686df57f5a09
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-11-20 18:45:16 -0300

    feat: valor min

--- scripts_node/novoValorArma.js
+++ scripts_node/novoValorArma.js
@@ -63,106 +63,106 @@ const fetchData = async (itemName, priceLimit) => {
 
 (async () => {
   try {
-    const acoIgneoResults = await fetchData('Aço Ígneo', 70000);
+    const acoIgneoResults = await fetchData('Aço Ígneo', 70000, 35000);
     console.log('Aço Ígneo Results:', acoIgneoResults);
-
-    const adagaDoPerseguidor = await fetchData('Adaga do Perseguidor', 140000);
+    
+    const adagaDoPerseguidor = await fetchData('Adaga do Perseguidor', 140000, 70000);
     console.log('Adaga do Perseguidor:', adagaDoPerseguidor);
-
-    const arcoDeCaça = await fetchData('Arco de Caça Ilusional', 700000);
+    
+    const arcoDeCaça = await fetchData('Arco de Caça Ilusional', 700000, 350000);
     console.log('Arco de Caça Ilusional Results:', arcoDeCaça);
-
-    const arcoDemoniacoResults = await fetchData('Arco Demoniaco', 140000);
+    
+    const arcoDemoniacoResults = await fetchData('Arco Demoniaco', 140000, 70000);
     console.log('Arco Demoniaco Results:', arcoDemoniacoResults);
-
-    const arcoGigante = await fetchData('Arco Gigante', 280000);
+    
+    const arcoGigante = await fetchData('Arco Gigante', 280000, 140000);
     console.log('Arco Gigante Results:', arcoGigante);
-
-    const arcoMisticoResults = await fetchData('Arco Mistico', 70000);
+    
+    const arcoMisticoResults = await fetchData('Arco Mistico', 70000, 35000);
     console.log('Arco Místico Results:', arcoMisticoResults);
-
-    const balistaIlusional = await fetchData('Balista Ilusional', 700000);
+    
+    const balistaIlusional = await fetchData('Balista Ilusional', 700000, 350000);
     console.log('Balista Ilusional Results:', balistaIlusional);
-
-    const bandagensLimpasResults = await fetchData('Bandagens Limpas', 70000);
+    
+    const bandagensLimpasResults = await fetchData('Bandagens Limpas', 70000, 35000);
     console.log('Bandagens Limpas Results:', bandagensLimpasResults);
-
-    const bandagensLimpasIluResults = await fetchData('Bandagens Limpas Ilu', 700000);
+    
+    const bandagensLimpasIluResults = await fetchData('Bandagens Limpas Ilu', 700000, 350000);
     console.log('Bandagens Limpas Ilu Results:', bandagensLimpasIluResults);
-
-    const bastaoAberracaoResults = await fetchData('Bastão da Aberração', 280000);
+    
+    const bastaoAberracaoResults = await fetchData('Bastão da Aberração', 280000, 140000);
     console.log('Bastão da Aberração Results:', bastaoAberracaoResults);
     
-    const bazerald = await fetchData('Bazerald Ilu', 700000);
+    const bazerald = await fetchData('Bazerald Ilu', 700000, 350000);
     console.log('Bazerald Ilu Results:', bazerald);
-
-    const brilhoDouradoIlusional = await fetchData('Brilho Dourado Ilusional', 2100000);
+    
+    const brilhoDouradoIlusional = await fetchData('Brilho Dourado Ilusional', 2100000, 1050000);
     console.log('Brilho Dourado Ilusional Results:', brilhoDouradoIlusional);
-
-    const espadaCromadaResults = await fetchData('Espada Cromada de duas mãos', 70000);
+    
+    const espadaCromadaResults = await fetchData('Espada Cromada de duas mãos', 70000, 35000);
     console.log('Espada Cromada de duas mãos Results:', espadaCromadaResults);
-
-    const espadaVeteranaResults = await fetchData('Espada Veterana', 35000);
+    
+    const espadaVeteranaResults = await fetchData('Espada Veterana', 35000, 17500);
     console.log('Espada Veterana Results:', espadaVeteranaResults);
-
-    const gladioDaNobrezaResults = await fetchData('Gládio da Nobreza', 280000);
+    
+    const gladioDaNobrezaResults = await fetchData('Gládio da Nobreza', 280000, 140000);
     console.log('Gládio da Nobreza Results:', gladioDaNobrezaResults);
-
-    const guardaChuvaAntiquadoResults = await fetchData('Guarda-Chuva Antiquado', 70000);
+    
+    const guardaChuvaAntiquadoResults = await fetchData('Guarda-Chuva Antiquado', 70000, 35000);
     console.log('Guarda-Chuva Antiquado Results:', guardaChuvaAntiquadoResults);
-
-    const katarPetaPurpuraResults = await fetchData('Katar da Pétala Purpura', 70000);
+    
+    const katarPetaPurpuraResults = await fetchData('Katar da Pétala Purpura', 70000, 35000);
     console.log('Katar da Pétala Purpura Results:', katarPetaPurpuraResults);
-
-    const laminaDosCeus = await fetchData('Lâmina dos Céus', 525000);
+    
+    const laminaDosCeus = await fetchData('Lâmina dos Céus', 525000, 262500);
     console.log('Lâmina dos Céus Results:', laminaDosCeus);
-
-    const laminaGemeaAzulResults = await fetchData('Lâmina Gêmea Azul', 280000);
+    
+    const laminaGemeaAzulResults = await fetchData('Lâmina Gêmea Azul', 280000, 140000);
     console.log('Lâmina Gêmea Azul Results:', laminaGemeaAzulResults);
-
-    const laminaGemeaVermelhaResults = await fetchData('Lâmina Gêmea Vermelha', 280000);
+    
+    const laminaGemeaVermelhaResults = await fetchData('Lâmina Gêmea Vermelha', 280000, 140000);
     console.log('Lâmina Gêmea Vermelha Results:', laminaGemeaVermelhaResults);
-
-    const lancagiganteResults = await fetchData('Lança Gigante', 280000);
+    
+    const lancagiganteResults = await fetchData('Lança Gigante', 280000, 140000);
     console.log('Lança Gigante Results:', lancagiganteResults);
-
-    const luvaDeComboIlusional = await fetchData('Luva de Combo Ilusional', 700000 );
+    
+    const luvaDeComboIlusional = await fetchData('Luva de Combo Ilusional', 700000, 350000);
     console.log('Luva de Combo Ilusional Results:', luvaDeComboIlusional);
-
-    const manjubaResults = await fetchData('Manjuba', 70000);
+    
+    const manjubaResults = await fetchData('Manjuba', 70000, 35000);
     console.log('Manjuba Results:', manjubaResults);
-
-    const marteloVeteranoResults = await fetchData('Martelo Veterano', 70000);
+    
+    const marteloVeteranoResults = await fetchData('Martelo Veterano', 70000, 35000);
     console.log('Martelo Veterano Results:', marteloVeteranoResults);
-
-    const microfoneFloralIguResults = await fetchData('Microfone Floral de Igu', 70000);
+    
+    const microfoneFloralIguResults = await fetchData('Microfone Floral de Igu', 70000, 35000);
     console.log('Microfone Floral de Igu Results:', microfoneFloralIguResults);
-
-    const pilares = await fetchData('Pilares', 140000);
+    
+    const pilares = await fetchData('Pilares', 140000, 70000);
     console.log('Pilares:', pilares);
-
-    const rosaLabaredaResults = await fetchData('Rosa Labareda', 70000);
+    
+    const rosaLabaredaResults = await fetchData('Rosa Labareda', 70000, 35000);
     console.log('Rosa Labareda Results:', rosaLabaredaResults);
-
-    const sabreSinoiteResults = await fetchData('Sabre Sinoite', 70000);
+    
+    const sabreSinoiteResults = await fetchData('Sabre Sinoite', 70000, 35000);
     console.log('Sabre Sinoite Results:', sabreSinoiteResults);
-
-    const shurikenDaNevasca= await fetchData('Shuriken da Nevasca', 700000);
+    
+    const shurikenDaNevasca = await fetchData('Shuriken da Nevasca', 700000, 350000);
     console.log('Shuriken da Nevasca Results:', shurikenDaNevasca);
-
-    const tabulaIlusional = await fetchData('Tábula Ilusional Ilusional', 700000);
+    
+    const tabulaIlusional = await fetchData('Tábula Ilusional Ilusional', 700000, 350000);
     console.log('Tábula Ilusional Results:', tabulaIlusional);
-
-    const taeGooLyeonIlusional = await fetchData('Tae Goo Lyeon Ilusional', 700000);
+    
+    const taeGooLyeonIlusional = await fetchData('Tae Goo Lyeon Ilusional', 700000, 350000);
     console.log('Tae Goo Lyeon Ilusional Results:', taeGooLyeonIlusional);
-
-    const tentaculoAfiadoResults = await fetchData('Tentáculo Afiado', 35000);
+    
+    const tentaculoAfiadoResults = await fetchData('Tentáculo Afiado', 35000, 17500);
     console.log('Tentáculo Afiado Results:', tentaculoAfiadoResults);
-
-    const ukuleleNovoOzResults = await fetchData('Ukulele do Novo Oz', 70000);
+    
+    const ukuleleNovoOzResults = await fetchData('Ukulele do Novo Oz', 70000, 35000);
     console.log('Ukulele do Novo Oz Results:', ukuleleNovoOzResults);
-
-    const varaSagradaResults = await fetchData('Vara Sagrada', 70000);
+    
+    const varaSagradaResults = await fetchData('Vara Sagrada', 70000, 35000);
     console.log('Vara Sagrada Results:', varaSagradaResults);
   } catch (error) {
     console.error('Error:', error);
